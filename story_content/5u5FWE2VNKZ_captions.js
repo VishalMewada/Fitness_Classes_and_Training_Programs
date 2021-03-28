968677cb4fb8e8a96f73ce6e62ff46565fa2615e
ByteTutorials <thebytetutorials@gmail.com>
2021-03-28 13:31:21 +0530

    V1.0

---
+++ story_content/5u5FWE2VNKZ_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.50.24668.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.597%0D%0ACongratulations!%20You%20have%20completed%20the%20basic%20exercises%20to%20keep%20you%20motivated.%20%0D%0A%0D%0A"}')
